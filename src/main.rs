b7699af58404c7b22ef8f5cef45bb40b348a3813
Otavio-burato <otavioburato42@gmail.com>
2020-11-20 13:35:22 -0300

    just exemple

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,20 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpServer, Responder};
 use serde::Serialize;
 
 #[derive(Serialize)]
-struct Test {
-    numero: f32,
+struct Response {
+    echo: String,
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/json", web::get().to(json))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(home))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn json() -> impl Responder {
-    web::Json(Test { numero: 42.3 })
+async fn home(req: String) -> impl Responder {
+    web::Json(Response { echo: req })
 }
